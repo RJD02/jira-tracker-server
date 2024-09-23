cba968968fb980ec0a238a365fc57a2492235f29
RJD02 <dulangeraviraj@gmail.com>
2024-09-23 22:12:40 +0530

    Revert "Update gitignore and add base url"
    This reverts commit 270880f5772903abf92deca80c521d45975c14a6.

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,6 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 
-app.get('/', (req, res) => { console.log('Hello'); res.json({ message: 'Working' }) })
 app.get('/salam', fetchJiraData)
 app.get('/star', fetchJiraData)
 
