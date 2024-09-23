621cc8147bbf0de60f8a01609b3957364e534911
RJD02 <dulangeraviraj@gmail.com>
2024-09-23 08:41:30 +0530

    Update salam config

--- src/config/salam/salam-config.ts
+++ src/config/salam/salam-config.ts
@@ -1,7 +1,7 @@
 import { JiraCredential } from "../../types/types";
 export { salamTeam } from "./salam-team";
 
-export const BASE_URL = process.env.SALAM_BASE_URL;
+export const BASE_URL = process.env.SALAM_BASE_URL || "url";
 export const salamCredentials: JiraCredential = {
     protocol: "https",
     host: process.env.SALAM_HOST || "error",
