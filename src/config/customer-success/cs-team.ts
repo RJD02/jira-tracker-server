7bce924b635623dc27860a008952bbf40162a7f4
RJD02 <dulangeraviraj@gmail.com>
2024-10-16 02:00:12 +0530

    Add Rushikesh to customer-success team

--- src/config/customer-success/cs-team.ts
+++ src/config/customer-success/cs-team.ts
@@ -1,42 +1,47 @@
 import { TeamMember } from "../../types/team";
 
 export const customerSuccessTeam: TeamMember[] = [
-  {
-    name: "Raviraj",
-    id: "712020:35c3157a-a8bd-4001-8451-9f5c626b729e",
-    role: "developer",
-  },
-  {
-    name: "Pawan",
-    id: "62bda5f98a35a8a83fb447cd",
-    role: "developer",
-  },
-  { name: "Farhan", id: "61d5bde3e67ea2006bda7094", role: "developer" },
-  { name: "Venkat", id: "6305d82c03058426d64af218", role: "developer" },
-  {
-    name: "Sandeep",
-    id: "6305dec8f35a006e1be6761b",
-    role: "developer",
-  },
-  { name: "Arjun", id: "60e5b4634134aa00693e42bf", role: "developer" },
-  {
-    name: "Preetam",
-    id: "712020:b7c05aa1-abf1-4d84-ae57-c2699ee06520",
-    role: "developer",
-  },
-  {
-    name: "Gaurav",
-    id: "712020:21e0e914-a82f-46be-88f5-304754f64149",
-    role: "developer",
-  },
-  {
-    name: "Varun T",
-    id: "712020:0701bbf0-a0d2-4b12-8591-0a729fa94436",
-    role: "developer",
-  },
-  {
-    name: "Pratiksha",
-    id: "5fca1945cbead5006996bf02",
-    role: "developer",
-  },
+    {
+        name: "Raviraj",
+        id: "712020:35c3157a-a8bd-4001-8451-9f5c626b729e",
+        role: "developer",
+    },
+    {
+        name: "Pawan",
+        id: "62bda5f98a35a8a83fb447cd",
+        role: "developer",
+    },
+    { name: "Farhan", id: "61d5bde3e67ea2006bda7094", role: "developer" },
+    { name: "Venkat", id: "6305d82c03058426d64af218", role: "developer" },
+    {
+        name: "Sandeep",
+        id: "6305dec8f35a006e1be6761b",
+        role: "developer",
+    },
+    { name: "Arjun", id: "60e5b4634134aa00693e42bf", role: "developer" },
+    {
+        name: "Preetam",
+        id: "712020:b7c05aa1-abf1-4d84-ae57-c2699ee06520",
+        role: "developer",
+    },
+    {
+        name: "Gaurav",
+        id: "712020:21e0e914-a82f-46be-88f5-304754f64149",
+        role: "developer",
+    },
+    {
+        name: "Varun T",
+        id: "712020:0701bbf0-a0d2-4b12-8591-0a729fa94436",
+        role: "developer",
+    },
+    {
+        name: "Pratiksha",
+        id: "5fca1945cbead5006996bf02",
+        role: "developer",
+    },
+    {
+        name: 'Rushikesh',
+        id: '712020:08dd5fdb-a31e-4a4a-bbf7-6dc0a3948344',
+        role: 'developer'
+    }
 ];
